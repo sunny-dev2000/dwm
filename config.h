ea66b0c4bf3bd9bfe4d94ec848eedf836ac239cc
sunny <sunny.dev@tutamail.com>
2025-11-08 14:07:44 +0100

    1. los gaps se acomodaron todos a 10 pxl de distancia.\n 2. Se cambiaron las fuentes por JetBrainsMono y NotoColorEmoji.\n 3.Se cambiaron los iconos de nerdfont por emojis en el escritorio.\n

--- config.h
+++ config.h
@@ -3,15 +3,15 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=13", "NotoColorEmoji:pixelsize=13:antialias=true:autohint=true", "monospace:size=13" };
+static const char dmenufont[]       = "NotoColorEmoji:size=13" ;
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -24,7 +24,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "🌐", "✨", "📖", "🎶", "💬", "🎨", "💀", "⚙️", "🔥" };
 
 static const Rule rules[] = {
 	/* xprop(1):
